ff7c4ba7cd54267384678027484deff32448abdd
Yifan Hong <elsk@google.com>
2017-09-23 03:46:00 +0530

    bacon: Uprev health HIDL to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- bacon.mk
+++ bacon.mk
@@ -112,7 +112,11 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.enable_boot_charger_mode=1
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
