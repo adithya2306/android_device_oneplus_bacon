c46d7d175ca6a25a7f6cae2c14cb9788ef313abd
Joey <joey@lineageos.org>
2018-01-24 11:24:37 +0100

    bacon: Build textclassifier's smart selection
    [@ghostrider-reborn: Rename package name for pie - author aviraxp]
    
    Co-authored-by: Han Wang <416810799@qq.com>

--- bacon.mk
+++ bacon.mk
@@ -249,6 +249,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine-8974.conf
