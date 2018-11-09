cd8d6ec3d73f7798bce66a3961754bbae12655ab
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-11-09 19:33:18 +0530

    bacon: Ship passthrough renderscript HIDL

--- bacon.mk
+++ bacon.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_oppo
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
