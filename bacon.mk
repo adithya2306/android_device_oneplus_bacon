3cbb3e1e82d1e93e933ba8cbceff55c6da189c4e
Bladeyy <Bladeyy@users.noreply.github.com>
2019-11-03 16:58:14 +0100

    bacon: Enable USB debugging and unsecure ADB by default

--- bacon.mk
+++ bacon.mk
@@ -285,6 +285,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.bacon
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
